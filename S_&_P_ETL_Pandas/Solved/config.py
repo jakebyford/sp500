a6275e0207ef369fd4c71c5ff93386bb6b0c0beb
Jake Byford <jake_byford@outlook.com>
2020-09-04 22:23:35 -0400

    Web HTML Design Foundation

---
+++ S_&_P_ETL_Pandas/Solved/config.py
@@ -0,0 +1,3 @@
+#  Add your API key
+api_key = "AKZD1D3S61SS4O9OEWKH"
+secret_key = "fxlignEhCjY2Fs2WO0IEkQv8gKRDE8FtCjHjMtxq"
---
+++ S_&_P_ETL_Pandas/Solved/sp500.sql
@@ -0,0 +1,40 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE main_data (
+ID TEXT PRIMARY KEY,
+	Symbol Text,
+	name TEXT,
+	last TEXT
+	);
+drop table main_data
+select * from main_data
+
+CREATE TABLE technical_data (
+ID TEXT PRIMARY KEY,
+	Symbol TEXT,
+	_52W_Low TEXT,
+	_52W_High TEXT
+	);
+drop table technical_data
+select * from technical_data
+
+CREATE TABLE fundamental_data (
+ID TEXT PRIMARY KEY,
+	Symbol TEXT,
+	Market_Cap TEXT,
+	PriceEarnings_ttm TEXT,
+	Div_Yield TEXT
+	);
+	
+drop table fundamental_data
+select * from fundamental_data
+
+CREATE TABLE performance_data (
+ID TEXT PRIMARY KEY,
+	Symbol TEXT,
+	YTD_PerChg TEXT,
+	_1M_PerChg TEXT,
+	_52W_PerChg TEXT
+	);
+
+drop table performance_data
+select * from performance_data
