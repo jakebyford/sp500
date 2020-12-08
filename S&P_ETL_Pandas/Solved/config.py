e398fcb5c10d7cf2aceb11492971ca3dee0e86a5
Jake Byford <jake_byford@outlook.com>
2020-12-07 20:06:48 -0500

    changepath name

--- S&P_ETL_Pandas/Solved/config.py
+++
@@ -1,3 +0,0 @@
-#  Add your API key
-api_key = "api_key_here"
-secret_key = "secret_key_here"
---
+++ SP500_ETL_Pandas/Q2_2020/Q2-2020-Solved/config.py
@@ -0,0 +1,3 @@
+#  Add your API key
+api_key = "AK1LRGLRFCRB23D42A68"
+secret_key = "f8vqKqShI2HbnBvneC2XjTdn04ZOOuzLc91D0fk9"
